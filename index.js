555850d2fe644ade6a486665e9298798e87c49b4
Tanner <Tanner@tleroy.com>
2022-04-04 16:28:09 -0400

    commit tree 1cfba38fadc09730b23098461bbaa7c935445425

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() + 1)
+
+let num3 = 54
+let num4 = 10
+
+let mod = num3 % num4
+
+let max = Math.max(5, 10, 12, 6, 20, 3)
